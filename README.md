c4789a3b2a33ae1e053200b348cf2fc7c346e9e0
伊藤健一 <itoukenichi@MacBook-Pro-Developer.local>
2020-08-12 21:26:04 +0900

    add tapping image with event

--- README.md
+++ README.md
@@ -6,64 +6,8 @@
 * React Native：済
 * Xamarin.Forms：済
 
-## タップイベント
+## 画像をタップしてイベント処理をする
 ### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## Page Navigation(ページ遷移)
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## Tab View(Tab Navigation)
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## Grid
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## ファイル分割、別ファイルの読み込み
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## ScrollView
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## Label or Imageをタップ(ボタン化)
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## freamwork提供のデータ永続化機能
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-## Sqlite
-### url
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
-
-## title
-### URL
-* Flutter：-
-* React Native：-
-* Xamarin.Forms：-
-
+* Flutter：済
+* React Native：済
+* Xamarin.Forms：済
--- xamarin.forms_project/xamarin.forms_project/MainPage.xaml.cs
+++ xamarin.forms_project/xamarin.forms_project/MainPage.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace xamarin.forms_project
@@ -13,6 +8,31 @@ namespace xamarin.forms_project
         public MainPage()
         {
             InitializeComponent();
+
+            int index = 1;
+
+            var image = new Image()
+            {
+                Source = ImageSource.FromResource("xamarin.forms_project.images.image" + String.Format("{0:D3}", index) + ".jpg"),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                WidthRequest = 200,
+                HeightRequest = 100,
+            };
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (sender, e) =>
+            {
+                index++;
+                if (index == 4)
+                {
+                    index = 1;
+                }
+                image.Source = ImageSource.FromResource("xamarin.forms_project.images.image" + String.Format("{0:D3}", index) + ".jpg");
+            };
+            image.GestureRecognizers.Add(tapGestureRecognizer);
+
+            Layout.Children.Add(image);
         }
     }
 }
